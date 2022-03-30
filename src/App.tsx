ec900eb5d4bc035ed06e76b7c61139f7379018d9
Shaun Saker <shaun.saker@timeular.com>
2022-03-30 18:39:13 +0200

    feature/add firebase app check

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,14 @@ import { Router } from './router';
 import { persistor, store } from './store';
 import { sentry } from './sentry';
 import './mixpanel';
+import { firebase } from '@react-native-firebase/app-check';
+import { Platform } from 'react-native';
+
+// TODO: SS test this in production on a real device (the docs are misleading)
+if (!__DEV__ && Platform.OS === 'android') {
+  // @ts-expect-error activate only expects an arg on iOS (which is activated by default)
+  firebase.appCheck().activate();
+}
 
 const App = (): ReactElement => {
   // uncomment this if you want to purge the store
