31e1e84ab8cd1099b0801fb1441ff042f31c112f
Shaun Saker <shaun.saker@timeular.com>
2022-04-23 05:57:26 +0200

    feature/add loading indicator to LotStats

--- src/components/LotStats/index.tsx
+++ src/components/LotStats/index.tsx
@@ -1,15 +1,20 @@
 import moment from 'moment';
 import React, { ReactElement } from 'react';
+import { ActivityIndicator } from 'react-native';
 import { useSelector } from 'react-redux';
 import { useTimer } from 'react-timer-hook';
 import styled from 'styled-components/native';
-import { selectActiveLot } from '../../store/lots/selectors';
+import {
+  selectActiveLot,
+  selectLotsDataLoading,
+} from '../../store/lots/selectors';
 import { Typography } from '../Typography';
 
 interface LotStatsProps {}
 
 export const LotStats = ({}: LotStatsProps): ReactElement => {
   const activeLot = useSelector(selectActiveLot);
+  const loading = useSelector(selectLotsDataLoading);
 
   const { hours, minutes, seconds } = useTimer({
     expiryTimestamp: moment(activeLot?.drawTime).toDate(),
@@ -38,10 +43,22 @@ export const LotStats = ({}: LotStatsProps): ReactElement => {
       <Typography>
         {hours}h {minutes}min {seconds}sec to go
       </Typography>
+
+      {loading && (
+        <ActivityIndicatorContainer>
+          <ActivityIndicator size="small" />
+        </ActivityIndicatorContainer>
+      )}
     </Container>
   );
 };
 
 const Container = styled.View`
   border-width: 1px;
+`;
+
+const ActivityIndicatorContainer = styled.View`
+  position: absolute;
+  top: 0;
+  right: 0;
 `;
